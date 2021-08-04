5cf0fbdc0b42ec8796ecdfd16c5f804cb5d479fb
Zinoe01 <87536069+Zinoe01@users.noreply.github.com>
2021-08-04 22:45:27 +0100

    add one navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeView(),
     );
   }
@@ -22,15 +22,21 @@ class HomeView extends StatelessWidget {
     return [
       Scaffold(),
       Scaffold(),
+      Scaffold(),
     ];
   }
-
 
   List<PersistentBottomNavBarItem> _navBarsItems() {
     return [
       PersistentBottomNavBarItem(
         icon: Icon(Icons.home),
         title: ("Home"),
+        // activeColorPrimary: CupertinoColors.activeBlue,
+        // inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.fastfood_outlined),
+        title: ("Order"),
         // activeColorPrimary: CupertinoColors.activeBlue,
         // inactiveColorPrimary: CupertinoColors.systemGrey,
       ),
@@ -57,10 +63,10 @@ class HomeView extends StatelessWidget {
       backgroundColor: Colors.white, // Default is Colors.white.
       handleAndroidBackButtonPress: true, // Default is true.
       resizeToAvoidBottomInset:
-      true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
       stateManagement: true, // Default is true.
       hideNavigationBarWhenKeyboardShows:
-      true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
       decoration: NavBarDecoration(
         borderRadius: BorderRadius.circular(10.0),
         colorBehindNavBar: Colors.white,
@@ -79,7 +85,7 @@ class HomeView extends StatelessWidget {
         duration: Duration(milliseconds: 200),
       ),
       navBarStyle:
-      NavBarStyle.style1, // Choose the nav bar style with this property.
+          NavBarStyle.style1, // Choose the nav bar style with this property.
     );
   }
 }
